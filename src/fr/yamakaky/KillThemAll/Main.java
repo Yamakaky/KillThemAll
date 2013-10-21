ab14e568763228c950440ab4c1f7dc7e49cd501b
Yamakaky <yamakaky@gmail.com>
2013-10-21 19:53:52 +0200

    Première version

---
+++ src/fr/yamakaky/KillThemAll/Main.java
@@ -0,0 +1,36 @@
+package fr.yamakaky.KillThemAll;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener{
+
+	@EventHandler
+	public void onKill(EntityDamageByEntityEvent e) {
+		if (e.getEntityType() != EntityType.PLAYER || e.getDamager().getType() != EntityType.PLAYER)
+			return;
+		
+		Player dead = (Player) e.getEntity();
+		
+		if (dead.getHealth() < e.getDamage())
+			return;
+		
+		Player killer = (Player) e.getDamager();
+		
+		transferXP(dead, killer);
+	}
+
+	private void transferXP(Player dead, Player killer) {
+		killer.giveExp(3);
+	}
+	
+	@EventHandler
+	public void onDeath(EntityDeathEvent e) {
+		e.setDroppedExp(0);
+	}
+}
